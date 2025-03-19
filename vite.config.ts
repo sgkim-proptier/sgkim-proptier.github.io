2c17cb1afb6f2138eb6d4997755ea6d388d16e25
김성관 <sgkim@proptier.co.kr>
2025-03-19 16:05:45 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite({ target: 'react', autoCodeSplitting: true }), react()],
-  base: "/net_m"
+  base: "/net_m/"
 });
