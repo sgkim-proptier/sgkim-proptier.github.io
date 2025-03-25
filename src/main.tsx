1f81417b2c71a38621530afe0999e5952cd7d9c3
seonggwan kim <sgkim@ekms.kr>
2025-03-26 08:41:11 +0900

    staletime

--- src/main.tsx
+++ src/main.tsx
@@ -23,7 +23,7 @@ const router = createRouter({
   defaultPreload: 'intent',
   // Since we're using React Query, we don't want loader calls to ever be stale
   // This will ensure that the loader is always called when the route is preloaded or visited
-  defaultPreloadStaleTime: 0,
+  defaultPreloadStaleTime: 5000,
   scrollRestoration: true,
   defaultViewTransition: {
     types: ['slide-right'],
